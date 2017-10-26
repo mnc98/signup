fea9a0e73aaad07e07c1de9a01c9bcf546d994e5
Michael N Cohen <mnc24@afsaccess2.njit.edu>
2017-10-26 12:00:05 -0400

    registration form

---
+++ form.php
@@ -0,0 +1,32 @@
+<form action="/action_page.php">
+  <div class="container">
+    <label><b>First Name</b></label>
+    <input type="text" placeholder="Enter First Name" name="first name" required>
+    <br/>
+
+    <label><b>Last Name</b></label>
+    <input type="text" placeholder="Enter Last Name" name="last name" required>
+    <br/>
+   
+    <label><b>Email</b></label>
+    <input type="text" placeholder="Enter Email Address" name="email" required>
+    <br/>
+
+    <label><b>Phone Number</b></label>
+    <input type="text" placeholder="Enter Phone Number" name="phone number" required>
+    <br/>
+    
+    <label><b>Date of Birth</b></label>
+    <input type="text" placeholder="Enter Date of Birth" name="birthday" required>
+    <br/>
+
+    <label><b>Gender</b></label>
+    <input type="text" placeholder="Enter Gender" name="gender" required>
+    <p>Please make sure all information is correct!</p>
+
+    <div class="clearfix">
+      <button type="button"  class="cancelbtn">Cancel</button>
+      <button type="submit" class="signupbtn">Sign Up</button>
+    </div>
+  </div>
+</form>
